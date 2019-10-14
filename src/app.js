34159e68ba8260a0204b426d129d67b8d6091218
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-10-14 10:20:26 -0700

    add app.js content, connect to routes.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const morgan = require('morgan');
+
+const router = require('./routes/routes.js');
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(router);
+
+// //TODO: REFACTOR TO INCLUDE DOTENV VARS
+// module.exports = {
+//   server : app,
+//   start : ( port ) => {
+//     app.listen( port, () => {
+//       console.log('rockin n rollin on:', port);
+//     });
+//   },
+// };
+
+app.listen(8080, () => {
+  console.log('Up : 8080');
+});
---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// /signin
+// /signout
+// /create-event
+// /get-events
+// /post-pictures
+// /get-pictures
+
+module.exports = pixeRouter;
